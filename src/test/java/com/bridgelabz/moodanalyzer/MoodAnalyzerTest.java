0c3aedde6ba40ccb00097d25949715a9ac431822
Shivam Mole <shivammole1713@gmail.com>
2024-09-20 14:53:19 +0530

    adding test case 1.1

--- src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -1,13 +1,16 @@
 package com.bridgelabz.moodanalyzer;
 
+import junit.framework.TestCase;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MoodAnalyzerTest {
-    @Test
-    public void moood(){
+public class MoodAnalyzerTest extends TestCase {
+        MoodAnalyzer mood=new MoodAnalyzer();
+        @Test
+    public void sad_should_return_sad(){
 
+            String msg="I am in sad mood";
+            assertEquals("SAD",mood.analyze_mood(msg));
     }
-
 }
