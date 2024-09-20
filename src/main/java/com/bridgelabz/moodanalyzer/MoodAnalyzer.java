77b8266c115d76f6a4c66faf68d0bb8a280a1ae5
Shivam Mole <shivammole1713@gmail.com>
2024-09-20 14:40:39 +0530

    Mood analyzer use case 1

--- src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -1,4 +1,24 @@
 package com.bridgelabz.moodanalyzer;
 
 public class MoodAnalyzer {
+
+    public MoodAnalyzer(){ // empty constructor
+
+    }
+
+    public String analyze_mood(String msg){
+
+        if(msg.toLowerCase().contains("sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
+
+    public static void main(String[] args) {
+        MoodAnalyzer mood=new MoodAnalyzer();
+        String msg="I am feeling sad";
+
+        System.out.println("mood according to the function is :- "+ mood.analyze_mood(msg));
+
+    }
 }
--- src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -1,4 +1,13 @@
+package com.bridgelabz.moodanalyzer;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MoodAnalyzerTest {
-  
+    @Test
+    public void moood(){
+
+    }
+
 }
